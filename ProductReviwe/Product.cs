c01b1a7cdc85f8246068803f4b41ed0aadb803d5
aakashpotdar <potdaraakash@gmail.com>
2021-12-14 13:57:26 +0530

    retriveing all recordes skip top 5 method is added

--- ProductReviwe/Product.cs
+++ ProductReviwe/Product.cs
@@ -9,7 +9,7 @@ namespace ProductReviwe
         public int ProductId { get; set; }
         public int UserId{get; set;}
         public string Review { get; set; }
-        public string isLike { get; set; }
+        public bool isLike { get; set; }
         public int Rating { get; set; }
     }
 }
--- ProductReviwe/Program.cs
+++ ProductReviwe/Program.cs
@@ -10,16 +10,16 @@ namespace ProductReviwe
             Console.WriteLine("Hello World!");
             List<Product> product = new List<Product>()
             {
-                new Product { ProductId = 001, UserId = 01, Review = "Good", isLike = "Good", Rating = 7 },
-            new Product() { ProductId = 002, UserId = 02, Review = "Good", isLike = "Good", Rating = 9 },
-            new Product(){ ProductId = 003, UserId = 03, Review = "Bad", isLike = "No", Rating = 2 },
-            new Product() { ProductId = 002, UserId = 04, Review = "Bad", isLike = "Good", Rating = 4 },
-            new Product() { ProductId = 005, UserId = 05, Review = "Bad", isLike = "Good", Rating = 5 },
-            new Product() { ProductId = 001, UserId = 06, Review = "Good", isLike = "Good", Rating = 6 },
-            new Product() { ProductId = 004, UserId = 07, Review = "Bad", isLike = "Good", Rating = 6 },
-            new Product() { ProductId = 002, UserId = 08, Review = "Good", isLike = "Good", Rating = 4 },
-            new Product() { ProductId = 002, UserId = 09, Review = "Bad", isLike = "No", Rating = 1 },
-            new Product() { ProductId = 04, UserId = 10, Review = "Bad", isLike = "Good", Rating = 2 },
+                new Product { ProductId = 001, UserId = 01, Review = "Good", isLike = true, Rating = 7 },
+            new Product() { ProductId = 002, UserId = 02, Review = "Good", isLike = true, Rating = 9 },
+            new Product(){ ProductId = 003, UserId = 03, Review = "Bad", isLike = false, Rating = 2 },
+            new Product() { ProductId = 002, UserId = 04, Review = "Bad", isLike = true, Rating = 4 },
+            new Product() { ProductId = 005, UserId = 05, Review = "Bad", isLike = true, Rating = 5 },
+            new Product() { ProductId = 001, UserId = 06, Review = "Good", isLike = true, Rating = 6 },
+            new Product() { ProductId = 004, UserId = 07, Review = "Bad", isLike = false, Rating = 6 },
+            new Product() { ProductId = 002, UserId = 08, Review = "Good", isLike = false, Rating = 4 },
+            new Product() { ProductId = 002, UserId = 09, Review = "Bad", isLike = false, Rating = 1 },
+            new Product() { ProductId = 04, UserId = 10, Review = "Bad", isLike = true, Rating = 2 },
         };
             ProductShowOprations productShow = new ProductShowOprations();
 
